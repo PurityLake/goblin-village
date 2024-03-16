82beff15021cc8f705add06591f0d5a2d938a294
Robert O'Shea <robertoshea2k11@gmail.com>
2024-03-16 02:44:52 +0000

    Update code to follow more modern libtcod

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,79 @@
+#include <SDL.h>
+
+#include <cstdlib>
+#include <filesystem>
+#include <iostream>
 #include <libtcod.hpp>
 
-int main(int argc, char **argv) {
-    TCODConsole::initRoot(80, 50, "libtcod C++ tutorial", false);
-    while(!TCODConsole::isWindowClosed()) {
-        TCOD_key_t key;
-        TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS, &key, nullptr);
-        TCODConsole::root->clear();
-        TCODConsole::root->putChar(40, 25, '@');
-        TCODConsole::flush();
+auto get_data_dir() -> std::filesystem::path {
+  static auto root_dir = std::filesystem::path{"."};
+
+  while (!std::filesystem::exists(root_dir / "data")) {
+    root_dir /= "..";
+    if (!std::filesystem::exists(root_dir)) {
+      throw std::runtime_error("Could not find the data directory.");
     }
-    return 0;
+  }
+
+  return root_dir / "data";
+}
+
+static constexpr auto WHITE = tcod::ColorRGB{255, 255, 255};
+
+static tcod::Console g_console;
+static tcod::Context g_context;
+
+void main_loop() {
+  g_console.clear();
+  tcod::print(g_console, {0, 0}, "Hello World", WHITE, std::nullopt);
+  g_context.present(g_console);
+
+  // handle event
+  SDL_Event event;
+#ifndef __EMSCRIPTEN__
+  SDL_WaitEvent(nullptr);
+#endif
+  while (SDL_PollEvent(&event)) {
+    switch (event.type) {
+      case SDL_QUIT:
+        std::exit(EXIT_SUCCESS);
+        break;
+    }
+  }
+}
+
+int main(int argc, char** argv) {
+  try {
+    g_console = tcod::Console{70, 40};
+    auto tileset = tcod::load_tilesheet(get_data_dir() / "dejavu16x16_gs_tc.png", {32, 8}, tcod::CHARMAP_TCOD);
+    // configure the context.
+    auto params = TCOD_ContextParams{};
+    params.window_title = "Libtcod Project";
+    params.tcod_version = TCOD_COMPILEDVERSION;
+    params.argc = argc;
+    params.argv = argv;
+    params.renderer_type = TCOD_RENDERER_SDL2;
+    params.sdl_window_flags = SDL_WINDOW_RESIZABLE;
+    params.vsync = true;
+    params.pixel_width = 800;
+    params.pixel_height = 600;
+    params.tileset = tileset.get();
+    params.console = g_console.get();
+
+    g_context = tcod::Context{params};
+
+#ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop(main_loop, 0, 0);
+#else
+    while (true) {
+      main_loop();
+    }
+#endif
+
+  } catch (const std::exception& e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return 0;
 }
